1d8cdec68bbfac2c4af7d4c7db01a6da3d2f03c5
A-Thiyagarajan <athiyagarajan01@gmail.com>
2024-10-07 09:44:28 +0530

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showPopup(id) {
+    document.getElementById(id).style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+  }
+  
+  function hidePopup(id) {
+    document.getElementById(id).style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+  }
+  
